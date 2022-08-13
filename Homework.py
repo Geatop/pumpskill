12554745559e1e53058809fb8ee5ffc8c31d1b1c
Geatop <94798832+Geatop@users.noreply.github.com>
2022-08-13 19:34:33 +0500

    First

--- Homework.py
+++ Homework.py
@@ -1,15 +1,15 @@
-# def fizz_buzz(a,b):
-#     sum_fizz_buzz = 0
-#     for i in range(a,b+1):
-#         if i % 3 == 0 and i % 5 == 0:
-#             sum_fizz_buzz += i 
-#     return sum_fizz_buzz        
+def fizz_buzz(a,b):
+    sum_fizz_buzz = 0
+    for i in range(a,b+1):
+        if i % 3 == 0 and i % 5 == 0:
+            sum_fizz_buzz += i 
+    return sum_fizz_buzz        
     
 
-# print('0-3:', fizz_buzz(0, 3))
-# print('0-5:', fizz_buzz(0, 5))
-# print('15-15:', fizz_buzz(15, 15))
-# print('1000-20000:', fizz_buzz(1000, 20000))
+print('0-3:', fizz_buzz(0, 3))
+print('0-5:', fizz_buzz(0, 5))
+print('15-15:', fizz_buzz(15, 15))
+print('1000-20000:', fizz_buzz(1000, 20000))
 
 def plural_form(number, form_1, form_2, form_3):
     final_form = ''
