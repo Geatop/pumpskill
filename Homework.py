a5331b4784241d8a03e7db983ea8a2ef12154571
Geatop <94798832+Geatop@users.noreply.github.com>
2022-08-13 18:47:31 +0500

    First

---
+++ Homework.py
@@ -0,0 +1,12 @@
+def fizz_buzz(a,b):
+    sum_fizz_buzz = 0
+    for i in range(a,b+1):
+        if i % 2 == 0 and i % 5 == 0:
+            sum_fizz_buzz += i 
+    return sum_fizz_buzz        
+    
+
+print('0-3:', fizz_buzz(0, 3))
+print('0-5:', fizz_buzz(0, 5))
+print('15-15:', fizz_buzz(15, 15))
+print('1000-20000:', fizz_buzz(1000, 20000))
