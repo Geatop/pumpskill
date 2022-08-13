192c830ce14bbaebc4fd78696e3b6c614d42df6c
Geatop <94798832+Geatop@users.noreply.github.com>
2022-08-13 18:51:48 +0500

    First

--- Homework.py
+++ Homework.py
@@ -1,7 +1,7 @@
 def fizz_buzz(a,b):
     sum_fizz_buzz = 0
     for i in range(a,b+1):
-        if i % 2 == 0 and i % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             sum_fizz_buzz += i 
     return sum_fizz_buzz        
     
