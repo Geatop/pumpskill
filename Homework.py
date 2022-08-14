dd791e16402737d404764b7f70d9fe321fc74a29
Geatop <94798832+Geatop@users.noreply.github.com>
2022-08-14 20:36:39 +0500

    'Zadacha2'

--- Homework.py
+++ Homework.py
@@ -18,17 +18,25 @@ def plural_form(number, form_1, form_2, form_3):
     numbers_for_form2 = [2,3,4]
     numbers_for_form3 = [0,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-    #Может быть попробовать через словарь типо форм 1: цифры такие то
-    
-    if int(numbers[-1]) in numbers_for_form1:
-        final_form = form_1
-    elif int(numbers[-1]) in numbers_for_form2:
-        final_form = form_2
-    elif int(numbers[-1]) in numbers_for_form3:
-        final_form = form_3
+    try:
+        if int(numbers[-2]) and int(numbers[-2]) == 1:
+            final_form = form_3
+        elif int(numbers[-2]) and int(numbers[-2]) != 1:
+            if int(numbers[-1]) in numbers_for_form1:
+                final_form = form_1
+            elif int(numbers[-1]) in numbers_for_form2:
+                final_form = form_2
+            elif int(numbers[-1]) in numbers_for_form3:
+                final_form = form_3
+    except Exception as E:
+        if int(numbers[-1]) in numbers_for_form1:
+                final_form = form_1
+        elif int(numbers[-1]) in numbers_for_form2:
+                final_form = form_2
+        elif int(numbers[-1]) in numbers_for_form3:
+                final_form = form_3
     return final_form
-
-
+ 
 print(1, plural_form(1, 'яблоко', 'яблока', 'яблок'))
 print(3, plural_form(3, 'яблоко', 'яблока', 'яблок'))
 print(5, plural_form(5, 'яблоко', 'яблока', 'яблок'))
