351c7ebda16b28c9b80b1dd1ff7f5c910005d043
Geatop <94798832+Geatop@users.noreply.github.com>
2022-08-15 19:16:26 +0500

    'Commit

--- Homework.py
+++ Homework.py
@@ -43,3 +43,29 @@ print(5, plural_form(5, 'яблоко', 'яблока', 'яблок'))
 print(11, plural_form(11, 'яблоко', 'яблока', 'яблок'))
 print(121, plural_form(121, 'яблоко', 'яблока', 'яблок'))
 print(125, plural_form(125, 'яблоко', 'яблока', 'яблок'))
+
+
+def html(nametag,**kwargs):
+    if kwargs:
+        final_par_parse = ''    
+        for ar in kwargs.items():
+            arlist = list(ar)
+            parametry = arlist[0] + '='+ f'"{str(arlist[1])}"' 
+            final_par_parse += parametry + ' '
+        final_par_parse = final_par_parse[:-1]
+        tag_with_kwargs = '<' + nametag + final_par_parse+ '>'
+
+    else:
+        return "<"+nametag+">"
+        # tag_with_kwargs = '<' + nametag + str(kwargs)+ '>'
+    return tag_with_kwargs
+# print (html('za', width=21, heyda=32))
+
+@html('body')
+@html('div', width=200, height=100)
+@html('a', href='https://yandex.ru/')
+def to_string(input_value):
+    return str(input_value)
+
+
+print(to_string('ссылка на яндекс'))
